2d509c5a4c278ea66566843d89e36c8c37ace9a9
zhuxiuwei <jlu_zxw@126.com>
2016-11-17 17:14:42 +0800

    a working example is added.

---
+++ HttpClientSample/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpClientSample
+{
+    class Program
+    {
+        //refer: https://www.asp.net/web-api/overview/advanced/calling-a-web-api-from-a-net-client
+        static HttpClient client = new HttpClient();
+
+        static void Main(string[] args)
+        {
+            RunAsync().Wait();
+        }
+
+        static void ShowAuthor(Author author)
+        {
+            Console.WriteLine($"Name: {author.Name}\t");
+        }
+
+        static async Task<Author> GetAuthorAsync(string path)
+        {
+            Author author = null;
+            HttpResponseMessage response = await client.GetAsync(path);
+            if (response.IsSuccessStatusCode)
+            {
+                author = await response.Content.ReadAsAsync<Author>();
+            }
+            return author;
+        }
+
+        static async Task<Uri> CreateAuthorAsync(Author author)
+        {
+            HttpResponseMessage response = await client.PostAsJsonAsync("api/authors", author);
+            response.EnsureSuccessStatusCode();
+
+            // Return the URI of the created resource.
+            return response.Headers.Location;
+        }
+
+        static async Task<Author> UpdateAuthorAsync(Author author)
+        {
+            HttpResponseMessage response = await client.PutAsJsonAsync($"api/authors/{author.Id}", author);
+            response.EnsureSuccessStatusCode();
+
+            // Deserialize the updated product from the response body.
+            author = await response.Content.ReadAsAsync<Author>();
+            return author;
+        }
+
+        static async Task<HttpStatusCode> DeleteAuthorAsync(int id)
+        {
+            HttpResponseMessage response = await client.DeleteAsync($"api/authors/{id}");
+            return response.StatusCode;
+        }
+
+        static async Task RunAsync()
+        {
+            client.BaseAddress = new Uri("http://localhost:64302/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            try
+            {
+                // Create a new author
+                Author author = new Author { Name = "Gizmo" };
+
+                var url = await CreateAuthorAsync(author);
+                Console.WriteLine($"Created at {url}. Press any key to continue");
+                Console.ReadKey();
+
+                // Get the author
+                author = await GetAuthorAsync(url.PathAndQuery);
+                ShowAuthor(author);
+
+                // Update the author
+                Console.WriteLine("Updating name...");
+                author.Name = "222222";
+                await UpdateAuthorAsync(author);
+                Console.WriteLine($"Updated name. Press any key to continue");
+                Console.ReadKey();
+
+                // Get the updated product
+                author = await GetAuthorAsync(url.PathAndQuery);
+                ShowAuthor(author);
+
+                // Delete the product
+                var statusCode = await DeleteAuthorAsync(author.Id);
+                Console.WriteLine($"Deleted (HTTP Status = {(int)statusCode})");
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Tests/TestWebAPI.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Text;
+using System.Threading.Tasks;
+using BookService.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class TestWebAPI
+    {
+        internal  string Serialize<T>(MediaTypeFormatter formatter, T value)
+        {
+            // Create a dummy HTTP Content.
+            Stream stream = new MemoryStream();
+            var content = new StreamContent(stream);
+            /// Serialize the object.
+            formatter.WriteToStreamAsync(typeof(T), value, stream, content, null).Wait();
+            // Read the serialized string.
+            stream.Position = 0;
+            return content.ReadAsStringAsync().Result;
+        }
+        internal  T Deserialize<T>(MediaTypeFormatter formatter, string str) where T : class
+        {
+            // Write the serialized string to a memory stream.
+            Stream stream = new MemoryStream();
+            StreamWriter writer = new StreamWriter(stream);
+            writer.Write(str);
+            writer.Flush();
+            stream.Position = 0;
+            // Deserialize to an object of type T
+            return formatter.ReadFromStreamAsync(typeof(T), stream, null, null).Result as T;
+        }
+        [TestMethod]
+        public  void TestSerialization()
+        {
+            var value = new Author() { Name = "Alice", Id = 23 };
+
+            var xml = new XmlMediaTypeFormatter();
+            string str = Serialize(xml, value);
+            Console.WriteLine(str);
+
+            var json = new JsonMediaTypeFormatter();
+            str = Serialize(json, value);
+            Console.WriteLine(str);
+
+            // Round trip
+            var value2 = Deserialize<Author>(json, str);
+            Console.WriteLine(value2);
+            Assert.AreEqual(value2.Name, "Alice");
+            Assert.AreEqual(value2.Id, 23);
+        }
+
+    }
+}
